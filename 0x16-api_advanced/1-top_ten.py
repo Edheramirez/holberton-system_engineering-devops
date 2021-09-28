7bbd64daea2a62186b284c77ab957d70039bc565
Edheramirez <edher.ramirez@outlook.es>
2021-09-27 22:57:54 -0500

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of the first 10 hot posts listed"""
+from requests import get
+
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """define recurse"""
+    if after is None:
+        return hot_list
+
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+
+    params = {
+        'limit': 100,
+        'after': after
+    }
+
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            hot_list.append(post.get("title"))
+
+    except:
+        return None
+
+    return recurse(subreddit, hot_list, after)
